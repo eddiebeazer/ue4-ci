b077ff2d34c210b0b53ae37d54c9037ff1e4647b
Edward Beazer <ebeazer@thedigitalsages.com>
2023-02-02 19:21:13 -0500

    Wip

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/eddiebeazer/ue4-ci
+
+go 1.18
+
+require (
+	github.com/stretchr/testify v1.8.1
+	github.com/urfave/cli/v2 v2.24.3
+	gopkg.in/ini.v1 v1.67.0
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ pkgs/jsonToXml/jsonToXml.go
@@ -0,0 +1,125 @@
+package jsonToXml
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"github.com/urfave/cli/v2"
+	"io/ioutil"
+)
+
+// XML for JUnit
+type testsuite struct {
+	Errors    int           `xml:"errors,attr"`
+	Failures  int           `xml:"failures,attr"`
+	Tests     int           `xml:"tests,attr"`
+	Skipped   int           `xml:"skipped,attr"`
+	Time      float64       `xml:"time,attr"`
+	Name      string        `xml:"name,attr"`
+	TimeStamp string        `xml:"timestamp,attr"`
+	TestCase  []interface{} `xml:"testcase"`
+}
+
+type TestSuccess struct {
+	ClassName string  `xml:"classname,attr"`
+	Name      string  `xml:"name,attr"`
+	Time      float64 `xml:"time,attr"`
+}
+
+type TestFailed struct {
+	ClassName string     `xml:"classname,attr"`
+	Name      string     `xml:"name,attr"`
+	Time      float64    `xml:"time,attr"`
+	Failure   FailureTag `xml:"failure,omitempty"`
+}
+
+type FailureTag struct {
+	Message       string `xml:"message,attr"`
+	Type          string `xml:"type,attr"`
+	OutputMessage string `xml:",innerxml"`
+}
+
+// UE4 JSON Test Unit Structure
+type AutomationTestJson struct {
+	Succeeded     int     `json:"succeeded"`
+	Failed        int     `json:"failed"`
+	TotalDuration float64 `json:"totalDuration"`
+	TimeStamp     string  `json:"reportCreatedOn"`
+	Tests         []Test  `json:"tests"`
+}
+
+type Test struct {
+	TestDisplayName string      `json:"testDisplayName"`
+	FullTestPath    string      `json:"fullTestPath"`
+	Duration        float64     `json:"Duration"`
+	Errors          int         `json:"errors"`
+	Entries         []TestEntry `json:"entries,omitempty"`
+}
+
+type TestEntry struct {
+	Event TestEvent `json:"event"`
+}
+
+type TestEvent struct {
+	Type    string `json:"type"`
+	Message string `json:"message"`
+}
+
+func ParseTestOutput(jsonFilePath string, outPath string, testSuiteName string) error {
+	jsonFile, err := ioutil.ReadFile(jsonFilePath)
+	if err != nil {
+		return cli.Exit(fmt.Errorf("error reading json file: %s", err), 1)
+	}
+
+	testResults := AutomationTestJson{}
+
+	err = json.Unmarshal(jsonFile, &testResults)
+	if err != nil {
+		return cli.Exit(fmt.Errorf("error unmarshalling json file: %s", err), 1)
+	}
+
+	var testCases []interface{}
+
+	for _, test := range testResults.Tests {
+		className := test.FullTestPath
+		name := test.TestDisplayName
+		time := test.Duration
+
+		// If the test fails, add in new failed test xml else add in standard success
+		if test.Errors > 0 {
+			testCases = append(testCases, &TestFailed{
+				ClassName: className,
+				Name:      name,
+				Time:      time,
+				Failure: FailureTag{
+					Message:       test.Entries[0].Event.Message,
+					Type:          test.Entries[0].Event.Type,
+					OutputMessage: test.Entries[0].Event.Message,
+				}})
+		} else {
+			testCases = append(testCases, &TestSuccess{
+				ClassName: className,
+				Name:      name,
+				Time:      time,
+			})
+		}
+	}
+
+	uatXml := testsuite{
+		Errors:    0,
+		Failures:  testResults.Failed,
+		Name:      testSuiteName,
+		Time:      testResults.TotalDuration,
+		Skipped:   0,
+		Tests:     len(testResults.Tests),
+		TestCase:  testCases,
+		TimeStamp: testResults.TimeStamp,
+	}
+
+	file, _ := xml.MarshalIndent(uatXml, "", " ")
+	jUnitXML := []byte(xml.Header + string(file))
+
+	_ = ioutil.WriteFile(outPath, jUnitXML, 0644)
+
+	return nil
+}
