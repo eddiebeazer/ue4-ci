c6b4dc6c648df34ee8f8edf90aa53bac3996ba1a
Edward Beazer <ebeazer@thedigitalsages.com>
2023-02-17 01:34:56 -0500

    Moved perforce out of sub commands

--- main.go
+++ main.go
@@ -84,54 +84,52 @@ func main() {
 				Usage: "Sets and gets the current project version from DefaultGame.ini (must be writable)",
 				Subcommands: []*cli.Command{
 					{
-						Name:  "set",
-						Usage: "Sets the project version to the inputted string",
-						Subcommands: []*cli.Command{
-							{
-								Name: "perforce",
-								Usage: "Sets the version using perforce style branching. ex - rel0.2, dev0.1, patch4.3 and task9.4 \n" +
-									"The ideal usage of this command is to save the current dev and rel versions as separate vars \n" +
-									"and then provide them as parameters.  The new sem ver version will be spit out at the end.",
-								Flags: []cli.Flag{
-									&cli.StringFlag{
-										Name:     "relVersion",
-										Aliases:  []string{"s"},
-										Value:    "",
-										Usage:    "Current release version of the project",
-										Required: true,
-									},
-									&cli.StringFlag{
-										Name:     "devVersion",
-										Aliases:  []string{"d"},
-										Value:    "",
-										Usage:    "Current dev version of the project",
-										Required: true,
-									},
-									&cli.StringFlag{
-										Name:     "branch",
-										Aliases:  []string{"b"},
-										Value:    "",
-										Usage:    "Branch of the build",
-										Required: true,
-									},
-									&cli.StringFlag{
-										Name:     "iniPath",
-										Aliases:  []string{"i"},
-										Value:    "",
-										Usage:    "file path to the DefaultGame ini file",
-										Required: true,
-									},
-								},
-								Action: func(cCtx *cli.Context) error {
-									devVersion := cCtx.String("devVersion")
-									relVersion := cCtx.String("relVersion")
-									branch := cCtx.String("branch")
-									defaultGameIniPath := cCtx.String("iniPath")
-									_, err := projectVersion.SetVersionWithPerforce(devVersion, relVersion, branch, defaultGameIniPath)
-									return err
-								},
+						Name: "perforce",
+						Usage: "Sets the version using perforce style branching. ex - rel0.2, dev0.1, patch4.3 and task9.4 \n" +
+							"The ideal usage of this command is to save the current dev and rel versions as separate vars \n" +
+							"and then provide them as parameters.  The new sem ver version will be spit out at the end.",
+						Flags: []cli.Flag{
+							&cli.StringFlag{
+								Name:     "relVersion",
+								Aliases:  []string{"r"},
+								Value:    "",
+								Usage:    "Current release version of the project",
+								Required: true,
+							},
+							&cli.StringFlag{
+								Name:     "devVersion",
+								Aliases:  []string{"d"},
+								Value:    "",
+								Usage:    "Current dev version of the project",
+								Required: true,
+							},
+							&cli.StringFlag{
+								Name:     "branch",
+								Aliases:  []string{"b"},
+								Value:    "",
+								Usage:    "Branch of the build",
+								Required: true,
+							},
+							&cli.StringFlag{
+								Name:     "iniPath",
+								Aliases:  []string{"i"},
+								Value:    "",
+								Usage:    "file path to the DefaultGame ini file",
+								Required: true,
 							},
 						},
+						Action: func(cCtx *cli.Context) error {
+							devVersion := cCtx.String("devVersion")
+							relVersion := cCtx.String("relVersion")
+							branch := cCtx.String("branch")
+							defaultGameIniPath := cCtx.String("iniPath")
+							_, err := projectVersion.SetVersionWithPerforce(devVersion, relVersion, branch, defaultGameIniPath)
+							return err
+						},
+					},
+					{
+						Name:  "set",
+						Usage: "Sets the project version to the inputted string",
 						Flags: []cli.Flag{
 							&cli.StringFlag{
 								Name:     "setVersion",
